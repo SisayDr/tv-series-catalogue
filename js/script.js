adff78900aa101ba6ffb12e9d90c163c365d2f30
Sisay Driba <sisaydriba1@gmail.com>
2023-04-03 13:17:12 -0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,402 @@
+var TVShows = [
+  {
+    Title: "Game of Thrones",
+    Genre: "Drama, Action, Fantasy",
+    "Aired on": "HBO, US",
+    Runtime: "60 min.",
+    Status: "Canceled/Ended",
+    "Created by": "David Benioff, D.B. Weiss",
+    Rating: "4.72/5 (1314 ratings)",
+    Description:
+      "This is an HBO adaptation of American author George R.R. Martin's epic fantasy novel series A Song of Ice and Fire. \n\nThe show explores a medieval-like fantasy world with its plethora of characters all struggling in the only game that matters - the Game of Thrones. And in this game you either win or you die, there is no middle ground.",
+    ImgSrc: "Game of Thrones.jpg",
+  },
+  {
+    Title: "The Walking Dead",
+    Genre: "Drama, Thriller, Horror/Supernatural",
+    "Aired on": "AMC, US at 9:00 pm",
+    Runtime: "60 min.",
+    Status: "Returning Series",
+    "Created by": "Frank Darabont",
+    Rating: "4.18/5 (645 ratings)",
+    Description:
+      "The Walking Dead tells the story of the months and years that follow after a zombie apocalypse.\n\nIt follows a group of survivors, led by former police officer Rick Grimes, who travel in search of a safe and secure home. As the world overrun by the dead takes its toll on the survivors, their interpersonal conflicts present a greater danger to their continuing survival than the walkers that roam the country.\n\nOver time, the characters are changed by the constant exposure to death and some grow willing to do anything to survive.\n\nThe show is based on a comic book series of the same name by Robert Kirkman.",
+    ImgSrc: "The Walking Dead.jpeg",
+  },
+  {
+    Title: "Arrow",
+    Genre: "Action, Super Heroes",
+    "Aired on": "The CW, US",
+    Runtime: "60 min.",
+    Status: "Canceled/Ended",
+    "Created by": "Greg Berlanti, Marc Guggenheim, Andrew Kreisberg",
+    Rating: "3.97/5 (614 ratings)",
+    Description:
+      "Arrow is a modern retelling of the adventures of legendary DC hero Green Arrow.\n\nAfter a violent shipwreck, billionaire playboy Oliver Queen was missing and presumed dead for five years before being discovered alive on a remote island in the Pacific. He returned home to Starling City, welcomed by his devoted mother Moira, beloved sister Thea and former flame Laurel Lance.\n\nWith the aid of his trusted chauffeur/bodyguard John Diggle, the computer-hacking skills of Felicity Smoak and the occasional, reluctant assistance of former police detective, now beat cop, Quentin Lance, Oliver has been waging a one-man war on crime.",
+    ImgSrc: "Arrow.jpg",
+  },
+  {
+    Title: "Suits",
+    Genre: "Drama, Financial/Business",
+    "Aired on": "USA, US",
+    Runtime: "45 min.",
+    Status: "Canceled/Ended",
+    "Created by": "Aaron Korsh",
+    Rating: "4.38/5 (462 ratings)",
+    Description:
+      "A corporate lawyer in Manhattan goes on a recruitment trip and decides to hire a driven college dropout.\n\nSuits delves into the fast-paced, high-stakes world of a top Manhattan corporate law firm where hotshot associate Harvey Specter makes a risky move by hiring Mike Ross a brilliant but unmotivated college dropout, as his associate. As he becomes enmeshed in this unfamiliar world, Mike relies heavily on the firm's best paralegal Rachel Zane and Harvey's no-nonsense assistant Donna Paulsen to help him serve justice. With a photographic memory and the street smarts of a hustler, Mike proves to be a legal prodigy despite the absence of bonafide legal credentials.",
+    ImgSrc: "Suits.jpg",
+  },
+  {
+    Title: "Lucifer",
+    Genre: "Drama, Action, Mystery, Thriller",
+    "Aired on": "Netflix, US at 3:00 am",
+    Runtime: "45 min.",
+    Status: "Returning Series",
+    "Created by": "Tom Kapinos",
+    Rating: "4.53/5 (782 ratings)",
+    Description:
+      'Based on the Vertigo characters, the TV series, written and executive produced by Kapinos, centers on "Lucifer" who, bored and unhappy as the Lord of Hell, resigns his throne and abandons his kingdom for the gorgeous, shimmering insanity of Los Angeles, where he opens an exclusive piano bar called Lux.',
+    ImgSrc: "Lucifer.jpg",
+  },
+  {
+    Title: "The 100",
+    Genre: "Drama, Family, Thriller",
+    "Aired on": "The CW, US at 8:00 pm",
+    Runtime: "60 min.",
+    Status: "Upcoming Season Premiere",
+    "Created by": "Jason Rothenberg",
+    Rating: "4.37/5 (480 ratings)",
+    Description:
+      "A post-apocalyptic drama about 100 juvenile delinquents that are released to the surface of a wildly changed Earth, with the survival of the human race entirely in their hands.",
+    ImgSrc: "The 100.jpg",
+  },
+  {
+    Title: "Supergirl",
+    Genre: "Drama, Action, Adventure, Super Heroes",
+    "Aired on": "The CW, US at 9:00 pm",
+    Runtime: "60 min.",
+    Status: "Running",
+    "Created by": "Greg Berlanti",
+    Rating: "3.95/5 (441 ratings)",
+    Description:
+      "Twenty-four-year-old Kara Zor-El, who was taken in by the Danvers family when she was 13 after being sent away from Krypton, must learn to embrace her powers after previously hiding them. The Danvers teach her to be careful with her powers, until she has to reveal them during an unexpected disaster, setting her on her journey of heroism.",
+    ImgSrc: "Supergirl.jpg",
+  },
+  {
+    Title: "Brooklyn Nine-Nine",
+    Genre: "Comedy",
+    "Aired on": "NBC, US at 8:30 pm",
+    Runtime: "25 min.",
+    Status: "Returning Series",
+    "Created by": "Michael Schur, Dan Goor",
+    Rating: "4.59/5 (372 ratings)",
+    Description:
+      "Detective Jake Peralta has a light-hearted approach to crime, but things change when he gets a new by-the-book boss.\n\nBrooklyn Nine-Nine is an ensemble comedy about a talented-but-carefree detective, a by-the-book police captain and their precinct colleagues. While based in the workplace, Brooklyn Nine-Nine is not really about the job - it's about the men and women behind the badge.",
+    ImgSrc: "Brooklyn Nine-Nine.jpg",
+  },
+  {
+    Title: "Modern Family",
+    Genre: "Comedy",
+    "Aired on": "ABC, US",
+    Runtime: "25 min.",
+    Status: "Canceled/Ended",
+    "Created by": "Christopher Lloyd, Steven Levitan",
+    Rating: "4.31/5 (273 ratings)",
+    Description:
+      "Three different but related families face trials and tribulations in their own uniquely comedic ways. \n\nThe mockumentary explores the many different types of a modern family through the stories of a gay couple, a straight couple and a multi-ethnic couple.",
+    ImgSrc: "Modern Family.jpg",
+  },
+  {
+    Title: "Homeland",
+    Genre:
+      "Drama, Family, Action, Crime, Thriller, Military/War, Romance/Dating, Politics, Current Events, Religion",
+    "Aired on": "Showtime, US at 9:00 pm",
+    Runtime: "50 min.",
+    Status: "Running",
+    "Created by": "Gideon Raff, Howard Gordon, Alex Gansa",
+    Rating: "4.3/5 (341 ratings)",
+    Description:
+      'Based on the Israeli version of "Prisoners of War." A U.S. soldier who was thought to have been killed in Iraq over 10 years ago who is now being questioned as to whether he is a member of a sleeper cell sent to cause a terrorist attack. ',
+    ImgSrc: "Homeland.jpg",
+  },
+  {
+    Title: "The Good Doctor",
+    Genre: "Drama, Medical",
+    "Aired on": "ABC, US at 10:00 pm",
+    Runtime: "60 min.",
+    Status: "Returning Series",
+    "Created by": "David Shore",
+    Rating: "4.6/5 (283 ratings)",
+    Description:
+      "The Good Doctor centers on a young surgeon with Savant Syndrome who is recruited into the pediatric surgical unit of a prestigious hospital. The question will arise - can a person who doesn't have the ability to relate to people actually save their lives?",
+    ImgSrc: "The Good Doctor.jpg",
+  },
+  {
+    Title: "Supernatural",
+    Genre: "Drama, Action, Adventure, Horror/Supernatural",
+    "Aired on": "The CW, US at 8:00 pm",
+    Runtime: "45 min.",
+    Status: "Returning Series",
+    "Created by": "Eric Kripke",
+    Rating: "4.58/5 (457 ratings)",
+    Description:
+      "This haunting series follows the Winchester brothers as they crisscross the lonely and mysterious back roads of the country in their '67 Chevy Impala, hunting down every evil supernatural force they encounter along the way.",
+    ImgSrc: "Supernatural.jpg",
+  },
+  {
+    Title: "Riverdale",
+    Genre: "Drama",
+    "Aired on": "The CW, US at 8:00 pm",
+    Runtime: "45 min.",
+    Status: "Running",
+    "Created by": "Greg Berlanti, Roberto Aguirre-Sacasa",
+    Rating: "4.11/5 (251 ratings)",
+    Description:
+      "This is a bold, subversive take on Archie, Betty, Veronica and their friends from the comics, exploring the surrealism of small-town life - the darkness and weirdness bubbling beneath Riverdale's wholesome facade.",
+    ImgSrc: "Riverdale.jpg",
+  },
+  {
+    Title: "Mr. Robot",
+    Genre: "Drama, Crime, Thriller",
+    "Aired on": "USA, US",
+    Runtime: "50 min.",
+    Status: "Canceled/Ended",
+    "Created by": "Sam Esmail",
+    Rating: "4.26/5 (420 ratings)",
+    Description:
+      "This is a drama about a young programmer, Elliot, who suffers from a debilitating anti-social disorder and decides he can only connect to people by hacking them. Elliot finds himself in the intersection between a cybersecurity firm he works for, his hacking exploits at night, and the underworld hacking organization trying to recruit him to help bring down the 1 percent.",
+    ImgSrc: "Mr. Robot.jpg",
+  },
+  {
+    Title: "Marvel's Daredevil",
+    Genre: "Drama, Action, Crime, Super Heroes",
+    "Aired on": "Netflix, US",
+    Runtime: "55 min.",
+    Status: "Canceled/Ended",
+    "Created by": "Drew Goddard",
+    Rating: "4.33/5 (345 ratings)",
+    Description:
+      "Matt Murdock, a Marvel Comics hero who's a lawyer by day but fights the bad guys at night to avenge the murder of his father, a boxer. But what sets Matt apart from most superheroes isn't his back story it's his handicap. Matt is blind, the result of a childhood accident that may have removed his ability to see but gifted him with heightened senses of touch, taste, hearing and smell.",
+    ImgSrc: "Marvel's Daredevil.jpg",
+  },
+  {
+    Title: "This is Us",
+    Genre: "Comedy, Drama",
+    "Aired on": "NBC, US at 9:00 pm",
+    Runtime: "60 min.",
+    Status: "Returning Series",
+    "Created by": "Dan Fogelman",
+    Rating: "4.51/5 (183 ratings)",
+    Description:
+      'Sometimes life will surprise you. Starring Mandy Moore ("A Walk to Remember"), Milo Ventimiglia ("Heroes," "Gilmore Girls") and Sterling K. Brown ("The People V. O.J. Simpson: American Crime Story)," this refreshingly honest and provocative series follows a unique ensemble whose paths cross and their life stories intertwine in curious ways. We find several of them share the same birthday, and so much more than anyone would expect. From the writer and directors of "Crazy, Stupid, Love." comes a smart, modern dramedy that will challenge your everyday presumptions about the people you think you know.',
+    ImgSrc: "This is Us.jpg",
+  },
+  {
+    Title: "Blindspot",
+    Genre: "Drama, Action, Crime",
+    "Aired on": "NBC, US at 9:00 pm",
+    Runtime: "45 min.",
+    Status: "Upcoming Season Premiere",
+    "Created by": "Martin Gero",
+    Rating: "3.97/5 (348 ratings)",
+    Description:
+      "The show centers on an FBI agent who is drawn into a complex conspiracy when a naked amnesiac is found in Times Square covered in fresh tattoos... including his name on her back.",
+    ImgSrc: "Blindspot.jpg",
+  },
+  {
+    Title: "Shameless",
+    Genre: "Comedy, Drama",
+    "Aired on": "Showtime, US at 9:00 pm",
+    Runtime: "60 min.",
+    Status: "Returning Series",
+    "Created by": "Paul Abbott",
+    Rating: "4.68/5 (228 ratings)",
+    Description:
+      "Set in working-class Chicago, the Gallagher family, a working class family of eight, must survive the ups and downs of today's recession. With a mother who is out of her element and an alcoholic father who usually ends up passed out on the living room floor, 18-year-old daughter Fiona is left with the task of keeping her five younger brothers and sisters on the straight and narrow.",
+    ImgSrc: "Shameless.jpg",
+  },
+  {
+    Title: "Breaking Bad",
+    Genre: "Drama",
+    "Aired on": "AMC, US",
+    Runtime: "60 min.",
+    Status: "Canceled/Ended",
+    "Created by": "Vince Gilligan",
+    Rating: "4.7/5 (223 ratings)",
+    Description:
+      "Bryan Cranston from Malcolm in the Middle stars in this drama focused on a mid-life crisis gone bad for a high school chemistry teacher who turns to a life of crime.",
+    ImgSrc: "Breaking Bad.jpg",
+  },
+  {
+    Title: "Power (2014)",
+    Genre: "Drama, Action, Crime, Thriller",
+    "Aired on": "Starz, US",
+    Runtime: "60 min.",
+    Status: "Canceled/Ended",
+    "Created by": "Courtney Kemp Agboh",
+    Rating: "4.58/5 (113 ratings)",
+    Description:
+      "The show tells the story of James \"Ghost\" St. Patrick, a wealthy New York City nightclub owner who caters to the city's elite. He wants to build an empire, turn the club into a Fortune 500 business, but there's just one problem: Ghost is living a double life. When he is not in the club, he is the kingpin of the most lucrative drug network in New York for a very high-level clientele.",
+    ImgSrc: "Power (2014).jpg",
+  },
+];
+
+//================================================================================//
+var container = document.getElementById("container");
+
+for (var i = 0; i < TVShows.length; i++) {
+  var tvshow = document.createElement("div");
+  tvshow.className = "TVShow";
+  tvshow.name = TVShows[i]["Title"];
+  container.appendChild(tvshow);
+
+  var poster = document.createElement("div");
+  poster.className = "poster";
+  tvshow.appendChild(poster);
+
+  var pic = document.createElement("img");
+  pic.src = "./images/" + TVShows[i]["ImgSrc"];
+  poster.appendChild(pic);
+
+  var cpx = document.createElement("h2");
+  cpx.innerHTML = "CappieX eNTERTAINMENT";
+  poster.appendChild(cpx);
+
+  var details = document.createElement("div");
+  details.className = "details";
+  tvshow.appendChild(details);
+
+  var title = document.createElement("h2");
+  title.className = "title";
+  title.innerHTML = TVShows[i]["Title"];
+  details.appendChild(title);
+
+  var genre = document.createElement("h4");
+  genre.className = "genre";
+  genre.innerHTML = TVShows[i]["Genre"];
+  details.appendChild(genre);
+
+  var rating = document.createElement("label");
+  rating.className = "rating";
+  rating.innerHTML = TVShows[i]["Rating"];
+  details.appendChild(rating);
+
+  var plt = document.createElement("h3");
+  plt.innerHTML = "plot Summary";
+  details.appendChild(plt);
+
+  var plot = document.createElement("p");
+  plot.className = "plot";
+  plot.innerHTML = TVShows[i]["Description"];
+  details.appendChild(plot);
+}
+//===========================Genre Options========================================//
+CommonGenres = [
+  "Action",
+  "Drama",
+  "Romance",
+  "Crime",
+  "Fantasy",
+  "Family",
+  "Super Heroes",
+];
+var sel = document.getElementById("byGenre");
+for (var i = 0; i < CommonGenres.length; i++) {
+  var opt = document.createElement("option");
+  opt.value = CommonGenres[i];
+  opt.innerHTML = CommonGenres[i];
+  sel.appendChild(opt);
+}
+//=============================display ABCD...====================================//
+var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var byTitle = document.getElementById("byTitle");
+var ul = byTitle.getElementsByTagName("ul")[0];
+for (var i = 0; i < letters.length; i++) {
+  btn = document.createElement("li");
+  btn.innerHTML = letters[i];
+  btn.setAttribute("onclick", "filterByTitle('" + letters[i] + "')");
+  ul.appendChild(btn);
+}
+var tvShows = document.getElementsByClassName("TVShow");
+//================Paginations====================//
+var pagination = document.getElementById("pagination");
+var numOfPaginations = (tvShows.length / 5).toFixed(0);
+for (var i = 1; i <= numOfPaginations; i++) {
+  var btn = document.createElement("button");
+  btn.setAttribute("onclick", 'paginate("' + i + '")');
+  btn.innerHTML = "<h3>" + i + "</h3>";
+  pagination.appendChild(btn);
+}
+
+//================================================================================//
+
+function hideTvShows() {
+  for (var i = 0; i < tvShows.length; i++) {
+    tvShows[i].style.display = "none";
+  }
+}
+function paginate(pageNum) {
+  hideTvShows();
+  if (pageNum * 6 - tvShows.length > 6) {
+    var Stop = tvShows.length;
+  } else {
+    var Stop = pageNum * 6;
+  }
+  for (var i = (pageNum - 1) * 6; i < Stop; i++) {
+    tvShows[i].style.display = "block";
+  }
+}
+paginate(1);
+function filterByTitle(letter) {
+  hideTvShows();
+  //display back the matches
+  var matches = 0;
+  for (var i = 0; i < tvShows.length; i++) {
+    var title = tvShows[i].getElementsByClassName("title")[0].innerHTML;
+    if (title[0] == letter) {
+      matches++;
+      tvShows[i].style.display = "block";
+    }
+  }
+
+  if (matches == 0) {
+    document.getElementById("noResults").style.display = "block";
+    document.getElementById("noResults").innerHTML =
+      "No Result found for " + letter;
+  } else {
+    document.getElementById("noResults").innerHTML = "";
+    document.getElementById("noResults").style.display = "none";
+  }
+}
+//================================================================================//
+function filterByGenre(genre) {
+  hideTvShows();
+  var genre;
+  selectedGenre = document.getElementById("byGenre").value;
+  if (selectedGenre == "Drama") {
+    genre = /Drama/;
+  } else if (selectedGenre == "Crime") {
+    genre = /Crime/;
+  } else if (selectedGenre == "Romance") {
+    genre = /Romance/;
+  } else if (selectedGenre == "Action") {
+    genre = /Action/;
+  } else if (selectedGenre == "Thriller") {
+    genre = /Thriller/;
+  } else if (selectedGenre == "Adventure") {
+    genre = /Adventure/;
+  } else if (selectedGenre == "Family") {
+    genre = /Family/;
+  } else if (selectedGenre == "Fantasy") {
+    genre = /Fantasy/;
+  } else if (selectedGenre == "Super Heroes") {
+    genre = /Super Heroes/;
+  }
+  for (var i = 0; i < tvShows.length; i++) {
+    console.log(selectedGenre);
+    var Genres = tvShows[i].getElementsByClassName("genre")[0].innerHTML;
+    if (selectedGenre == "All" || genre.test(Genres))
+      tvShows[i].style.display = "block";
+  }
+}
